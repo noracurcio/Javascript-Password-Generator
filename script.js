c524a3e8ebebb4eff779a72f9b3be1b5f3b0ec07
noracurcio <noracurcio@gmail.com>
2020-09-24 20:35:33 -0600

    Changed from a while loop to a if else statement.

--- script.js
+++ script.js
@@ -10,5 +10,47 @@ function writePassword() {
 
 }
 
+// add variables for numbers
+// add variables for upper case
+// add variables for lower case
+// add special character
+
+
+  var upperCase = []
+  var lowerCase = []
+  var specialChar = []
+  var numbers = []
+ 
+
+var confirmLength = "";
+var confirmUpper = "";
+var confirmLower = "";
+var confirmSpecialChar = "";
+
+function generatePassword(){
+  var confirmLength = prompt("How long would you like your password to be? Please choose a length between 8 and 128 characters")
+  
+  while (confirmLength < 8 || confirmLength > 128 ){
+    var confirmLength = alert("Please choose a length between 8 and 128 characters");
+
+  }
+
+  if(confirmLength > 8 || confirmLength < 128 ) {
+
+  confirmUpper = confirm("WOuld you like to use upper case letters?");
+
+  }
+}
+
+generatePassword()
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// 
+
+//var for the answer for 
